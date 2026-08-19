bed776f9806b8daca67c70a45628958901da2de8
hyuckjin.kwon <hyuckjin.kwon@parksystems.com>
2026-08-19 11:24:28 +0900

    feat: add ISO 4287 profile roughness, the first curve-consuming operation

--- src/SmartAnalysis.Analysis/Operations/Image/ImageAnalysisModule.cs
+++ src/SmartAnalysis.Analysis/Operations/Image/ImageAnalysisModule.cs
@@ -28,6 +28,7 @@ public static class ImageAnalysisModule
         services.AddAnalysisOperation<PowerSpectrumOperation>();
         services.AddAnalysisOperation<ProfileOperation>();
         services.AddAnalysisOperation<LineProfileOperation>();
+        services.AddAnalysisOperation<ProfileRoughnessOperation>();
         return services;
     }
 }
